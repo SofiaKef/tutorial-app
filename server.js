34c3705734842183c7b50b62a3e27a276378a25c
SofiaKef <68301861+SofiaKef@users.noreply.github.com>
2020-07-28 14:08:47 +0300

    ...

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 5010;
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'pic-resize/public')));
@@ -10,13 +9,17 @@ app.use(express.static(path.join(__dirname, 'pic-resize/public')));
 // Handles any requests that don't match the ones above
 app.get('/resize', (req, res) => {
   res.send('Express is working');
+<<<<<<< HEAD
   const exampleJson = [
     { id: 1, desc: 'background' },
     { id: 2, desc: 'witch' },
     { id: 3, desc: 'vampire' },
   ];
+=======
+>>>>>>> parent of 49adc53... Made react fetch json data from express
 });
 
+const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log(`App is listening on port ${port}`);
