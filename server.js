9ef61cca303cc59513d3f4606332a9be5138b29b
SofiaKef <68301861+SofiaKef@users.noreply.github.com>
2020-07-28 11:37:09 +0300

    Added eslint

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'pic-resize/public')));
 
 // Handles any requests that don't match the ones above
-app.get('/resize', (req,res) =>{
-    res.send('Express is working');
+app.get('/resize', (req, res) => {
+  res.send('Express is working');
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('App is listening on port ' + port);
+console.log(`App is listening on port ${port}`);
